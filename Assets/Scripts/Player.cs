82a20b9a6dcac7e8e3d260983564bb2845575a94
physiocheck <matthieu.kern@physiocheck.eu>
2016-06-04 16:57:56 +0200

    Add project bases

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	bool isFalling = false;
+	Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rigidbody.IsSleeping()) {
+			rigidbody.WakeUp();
+		}
+
+		if (Input.GetKey (KeyCode.Space) && !isFalling) {
+			rigidbody.velocity = new Vector3(0, 7, 0);
+		}
+
+		isFalling = true;
+	}
+
+	void OnCollisionStay(Collision collisionInfo) {
+		isFalling = false;
+	}
+}
